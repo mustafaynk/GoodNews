34fbc224809f04413f9790386724c3ac0847756d
mustafaynk <m.ynk4236@gmail.com>
2022-11-12 13:30:25 +0300

    Kotlin refactor

---
+++ app/src/main/java/com/ynk/goodnews/utils/glidessl/MyGlideModule.kt
@@ -0,0 +1,23 @@
+package com.ynk.goodnews.utils.glidessl
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.module.AppGlideModule
+import java.io.InputStream
+
+@GlideModule
+class MyGlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+    }
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val okHttpClient = UnsafeOkHttpClient.unsafeOkHttpClient
+        registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(okHttpClient))
+    }
+}
